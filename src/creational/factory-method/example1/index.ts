23336a679e0e34ed4c95a20d54fde5bc023be73c
yigittopm <yigittopm@hotmail.com>
2022-03-05 15:53:46 +0300

    first commit

---
+++ src/creational/factory-method/example1/index.ts
@@ -0,0 +1,60 @@
+abstract class Creator {
+
+    public abstract factoryMethod(): Product;
+
+    public someOperation(): string {
+        const product = this.factoryMethod()
+        
+        return `Creator: The same creator's code has just worked with ${product.operation()}`;
+    }
+}
+
+class ConcreteCreator1 extends Creator {
+
+    public factoryMethod(): Product {
+        return new ConcreateProduct1();
+    }
+
+}
+
+class ConcreteCreator2 extends Creator {
+
+    public factoryMethod(): Product {
+        return new ConcreateProduct2();
+    }
+
+}
+
+/**
+ * 
+ *  Product implementation
+ * 
+ */
+
+interface Product {
+    operation(): string
+}
+
+class ConcreateProduct1 implements Product {
+    operation(): string {
+        return '{Result of the ConcreteProduct1}';
+    }
+}
+
+class ConcreateProduct2 implements Product {
+    operation(): string {
+        return '{Result of the ConcreteProduct2}';
+    }
+}
+
+function clientCode(creator: Creator) {
+    console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
+    console.log(creator.someOperation())
+}
+
+console.log('App: Launched with the ConcreteCreator1.');
+clientCode(new ConcreteCreator1());
+console.log('');
+
+console.log('App: Launched with the ConcreteCreator2.');
+clientCode(new ConcreteCreator2());
